b3f13310d98015b139272ea6208ce3dac8a52e64
Shuailin Li <shuailin@goairmart.com>
2025-01-14 18:30:32 +0800

    fix: static assets path prefix add

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+  trailingSlash: true,
+  basePath: "/mySwap", // 替换为你的 GitHub 仓库名
+  assetPrefix: "/mySwap", // 同样替换为你的 GitHub 仓库名
 };
 
 export default nextConfig;
